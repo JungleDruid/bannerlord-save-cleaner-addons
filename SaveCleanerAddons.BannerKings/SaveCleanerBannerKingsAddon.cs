81ff2778753e29785e9cbba9ab24f492ab66b0c1
JungleDruid <3953982+JungleDruid@users.noreply.github.com>
2025-02-09 12:15:00 +0800

    Add Banner Kings addon

---
+++ SaveCleanerAddons.BannerKings/SaveCleanerBannerKingsAddon.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using BannerKings.Behaviours.Diplomacy.Groups;
+using BannerKings.Behaviours.Relations;
+using BannerKings.Managers.Court;
+using BannerKings.Managers.Populations.Estates;
+using Microsoft.Extensions.Logging;
+using SaveCleaner;
+using TaleWorlds.CampaignSystem;
+
+namespace SaveCleanerAddons.BannerKings;
+
+internal static class SaveCleanerBannerKingsAddon
+{
+    internal static void Register()
+    {
+        var addon = new SaveCleanerAddon("SaveCleanerAddons.BannerKings", "BannerKings");
+        addon.CanRemoveChild += CanRemoveChild;
+        addon.DoRemoveChild += DoRemoveChild;
+        addon.AddSupportedNamespace(new Regex(@"^BannerKings\b"));
+        addon.Register<SubModule>();
+    }
+
+    private static bool CanRemoveChild(SaveCleanerAddon addon, Node node)
+    {
+        return RemoveChild(addon, node, true);
+    }
+
+    private static bool DoRemoveChild(SaveCleanerAddon addon, Node node)
+    {
+        return RemoveChild(addon, node, false);
+    }
+
+    private static bool RemoveChild(SaveCleanerAddon addon, Node node, bool dryRun)
+    {
+        bool result = false;
+        try
+        {
+            object obj = node.Value;
+            object parent = node.Parent.Value;
+            object top = node.Top.Value;
+            if (obj is Hero hero)
+            {
+                switch (top)
+                {
+                    case HeroRelations relations when relations.Hero == hero:
+                        // the HeroRelations object will be removed anyway
+                        result = true;
+                        break;
+                    case HeroRelations:
+                    {
+                        if (parent is Dictionary<Hero, List<RelationsModifier>> dictionary)
+                        {
+                            if (!dryRun) dictionary.Remove(hero);
+                            result = true;
+                        }
+
+                        break;
+                    }
+                    case BKRelationsBehavior:
+                    {
+                        if (parent is Dictionary<Hero, HeroRelations> dictionary)
+                        {
+                            if (!dryRun) dictionary.Remove(hero);
+                            result = true;
+                        }
+
+                        break;
+                    }
+                    case CouncilMember councilMember:
+                    {
+                        if (councilMember.Member == hero)
+                        {
+                            if (!dryRun)
+                            {
+                                councilMember.SetMember(null);
+                            }
+
+                            result = true;
+                        }
+
+                        break;
+                    }
+                    case Estate estate:
+                    {
+                        if (estate.Owner == hero)
+                        {
+                            if (!dryRun) estate.SetOwner(null);
+                            result = true;
+                        }
+
+                        break;
+                    }
+                    case DiplomacyGroup diplomacyGroup:
+                    {
+                        if (diplomacyGroup.JoinTime.ContainsKey(hero))
+                        {
+                            if (!dryRun) diplomacyGroup.JoinTime.Remove(hero);
+                            result = true;
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            addon.Log("Error removing child.", LogLevel.Error, ex);
+            result = false;
+        }
+
+        if (!result)
+        {
+#if DEBUG
+            addon.Log($"Unable to remove {GetLogName(node.Value)} from {GetLogName(node.Parent?.Value)}. Link: {node.GetLinkString()}", LogLevel.Warning);
+#else
+            addon.Log($"Unable to remove {GetLogName(node.Value)} from {GetLogName(node.Parent?.Value)}.", LogLevel.Debug);
+#endif
+        }
+
+        return result;
+    }
+
+    private static string GetLogName(object obj) => $"[{obj?.GetType().Name}]{obj}";
+}
---
+++ SaveCleanerAddons.BannerKings/SubModule.cs
@@ -0,0 +1,13 @@
+﻿using TaleWorlds.MountAndBlade;
+
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace SaveCleanerAddons.BannerKings;
+
+public class SubModule : MBSubModuleBase
+{
+    protected override void OnSubModuleLoad()
+    {
+        SaveCleanerBannerKingsAddon.Register();
+    }
+}
